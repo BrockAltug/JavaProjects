6a7a5794fc178649d8b8040f713e4c387a35f8fc
Brock <brock.altug99@gmail.com>
2023-03-01 14:39:19 -0800

    assignment 1 - print the letter Q

--- src/Project_1/Assignment_1.java
+++ src/Project_1/Assignment_1.java
@@ -1,4 +1,10 @@
 package Project_1;
 
-public class Assignment_1 {
+public class Assignment_1
+{
+    //Rearrange the following code so that it forms the correct program and prints the letter 'Q'
+
+    public static void main(String[] args) {
+        System.out.println('Q');
+    }
 }
