a610ab21349a1911571d1b561d23971bce1a7908
Brock <brock.altug99@gmail.com>
2023-03-01 15:18:26 -0800

    assignment 8 - combine 2 String values with 1 user input

---
+++ src/Project_1/Assignment_8.java
@@ -0,0 +1,19 @@
+package Project_1;
+
+import java.util.Scanner;
+
+public class Assignment_8
+{
+    //Assume the word is a string variable and already has a value
+    //write a statement to display the message "Today's-Of-The-Day is:" followed by
+    //the value of a word from users input
+    //the message and value should appear on a single line
+    //"Today's-Of-The-Day is:(user input)"
+    public static void main(String[] args) {
+        //create a scanner for user input
+        Scanner input = new Scanner(System.in);
+        String word1 = "Today's-Of-The-Day is:";
+        String word2 = input.nextLine();
+        System.out.println(word1+word2);
+    }
+}
