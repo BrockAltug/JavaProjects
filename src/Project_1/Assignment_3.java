db09e223201b478205923d5aa5790afc6b1ff32f
Brock <brock.altug99@gmail.com>
2023-03-01 14:51:18 -0800

    assignment 3 - print a code to make the shape of a square in the console

---
+++ src/Project_1/Assignment_3.java
@@ -0,0 +1,24 @@
+package Project_1;
+
+public class Assignment_3
+{
+    //write a java statement that prints the following square into the console
+    //********
+    //*      *
+    //*      *
+    //*      *
+    //*      *
+    //*      *
+    //*      *
+    //********
+    public static void main(String[] args) {
+        System.out.println("********");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("*      *");
+        System.out.println("********");
+    }
+}
