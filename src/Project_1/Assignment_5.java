280870b3482b978f708423b5d582ca6721435ce2
Brock <brock.altug99@gmail.com>
2023-03-01 15:00:13 -0800

    assignment 5 - string variable displayed as value

---
+++ src/Project_1/Assignment_5.java
@@ -0,0 +1,12 @@
+package Project_1;
+
+public class Assignment_5
+{
+    //Assume the message is a string variable, write a statement to display its value and only its value
+    //(nothing else) to the console/screen
+    //String message = "I am an Automation Engineer.";
+    public static void main(String[] args) {
+        String message = "I am an Automation Engineer.";
+        System.out.println(message);
+    }
+}
