e90dab344d747e9af0b3b5b661e3feef6d04bbf2
Brock <brock.altug99@gmail.com>
2023-03-01 16:30:08 -0800

    assignment 02 - analyze the following code and guess the output

---
+++ src/Project_2/Assignment_02.java
@@ -0,0 +1,19 @@
+package Project_2;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class Assignment_02
+{
+    //thinking practice
+    //analyze the following code and guess the output
+    public static void main(String[] args) {
+        int num = 7;
+        num += 5;      //num is 12
+        num -= 3;      //num is 9
+        num *= 4;      //num is 36
+        num /= 2;      //num is 18
+        num %= 10;     //num is 8
+        //correct output = 8
+        System.out.println(num);
+    }
+}
