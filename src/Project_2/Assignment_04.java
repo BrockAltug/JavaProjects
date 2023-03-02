7cf7da3bcbd61b3e2b1b073665ba51b1040fff53
Brock <brock.altug99@gmail.com>
2023-03-01 19:18:52 -0800

    assignment 04 - analyze the following code and guess the output pt.3

---
+++ src/Project_2/Assignment_04.java
@@ -0,0 +1,13 @@
+package Project_2;
+
+public class Assignment_04
+{
+    //analyze the following code and guess the output pt.3
+    public static void main(String[] args) {
+        int a = 7;
+        a *= 4/2;    //a = 14
+        --a;         //a = 13
+        a += 3+1;    //a = 17
+        System.out.println(a);
+    }
+}
