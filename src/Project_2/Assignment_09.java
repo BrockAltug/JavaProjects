aaa9b0f3bc39edd5a01876ad78b376a2b8d3e519
Brock <brock.altug99@gmail.com>
2023-03-02 20:13:44 -0800

    assignment 09 - practice creating classes with variables

---
+++ src/Project_2/Assignment_09.java
@@ -0,0 +1,21 @@
+package Project_2;
+//create 3 classes and add 2 appropriate variables for each class
+class Student{  //class created
+    private int studentId;   //int variable studentId added
+    private String studentName;   //String variable studentName added
+}
+
+class House{   //class created
+    private int houseNumber;   //int variable houseNumber added
+    private String address;   //String variable address added
+}
+
+class Employee{   //class Employee created
+    private int employeeId;   //int variable employeeId created
+    private String employeeName;   //String variable employeeName created
+}
+
+public class Assignment_09
+{
+
+}
